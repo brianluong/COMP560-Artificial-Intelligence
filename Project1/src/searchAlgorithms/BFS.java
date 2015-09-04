18de51146693f841976c181c65ab81d0b9d364a1
Brian Luong <bluong@live.unc.edu>
2015-09-04 17:48:23 -0400

    Implemented BFS algorithm

--- Project1/src/searchAlgorithms/BFS.java
+++ Project1/src/searchAlgorithms/BFS.java
@@ -1,5 +1,46 @@
 package searchAlgorithms;
 
-public class BFS {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
+public class BFS extends Search{
+	// Breadth First Search 
+	// FIFO queue
+	// look in four directions. if not a % sign, then add to frontier
+	// when node added to queue, check if it's goal
+	
+	public static List<Index> search(char[][] maze) {
+		Index starting = getStartingIndex(maze);
+		Map<Index, Index[]> map = generateAdjacencyList(maze);
+		Queue<Index> frontier = new LinkedList<>();
+		Set<Index> alreadyVisited = new HashSet<>();
+		List<Index> solutionPath = new ArrayList<>();
+		
+		frontier.add(starting);
+		while (frontier.size() > 0) {
+			Index expand = frontier.poll();
+			alreadyVisited.add(expand);
+			Index[] adjNodes = map.get(expand);
+			
+			for (Index i : adjNodes) {
+				if (!alreadyVisited.contains(i)) {
+					i.prev = expand;
+					if (isGoal(i, maze)) {
+						for (Index p = i; p != null; p = p.prev) {
+							solutionPath.add(new Index(p.row, p.column, null));	
+						}
+					} else {
+						frontier.add(i);
+					}	
+				}
+			}
+		}
+		return solutionPath;
+	}
 }
---
+++ Project1/src/searchAlgorithms/Index.java
@@ -0,0 +1,27 @@
+package searchAlgorithms;
+
+public class Index {
+	int row;
+	int column;
+	Index prev;
+	
+	public Index(int r, int c, Index p) {
+		row = r;
+		column = c;
+		prev = p;
+	}
+	
+	@Override
+	public int hashCode() {
+		return (int) (Math.pow(this.row, 7) + this.column*4734);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this.row == ((Index) o).row && this.column == ((Index) o).column) {
+			return true;
+			
+		} 
+		return false;
+	}
+}
